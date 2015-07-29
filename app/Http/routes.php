89f1b7781b58daceef795ee6baf8f5a8e8666a37
stvo <stijnvanobberghen@stvo.local>
2015-07-29 21:18:44 +0200

    create contact route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,4 +11,6 @@ Route::get('about', function () {
 Route::get('login', function () {
     return "you can login here if you want!";
 });
-
+Route::get('contact', function () {
+    return "you can login here if you want!";
+});
